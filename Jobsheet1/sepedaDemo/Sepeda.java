2ef94f5fcae47d9775ff420f38384b5b94b60ff0
RAFI ODY PRASETYO <rafiody16@gmail.com>
2024-09-01 14:11:55 +0700

    Jobsheet1

---
+++ Jobsheet1/sepedaDemo/Sepeda.java
@@ -0,0 +1,34 @@
+// package Jobsheet1.sepedaDemo;
+
+/**
+ * Sepeda
+ */
+public class Sepeda {
+
+    private String merek;
+    private int kecepatan;
+    private int gear;
+
+    public void setMerek(String newValue) {
+        merek = newValue;
+    }    
+
+    public void gantiGear(int newValue) {
+        gear = newValue;
+    }
+
+    public void tambahKecepatan(int increment) {
+        kecepatan += increment;
+    }
+
+    public void rem(int decrement) {
+        kecepatan -= decrement;
+    }
+
+    public void cetakStatus() {
+        System.out.println("Merek: "+merek);
+        System.out.println("Kecepatan: "+kecepatan);
+        System.out.println("Gear: "+gear);
+    }
+    
+}
---
+++ Jobsheet1/sepedaDemo/SepedaDemo.java
@@ -0,0 +1,23 @@
+// package Jobsheet1.sepedaDemo;
+
+public class SepedaDemo {
+    
+    public static void main(String[] args) {
+        
+        Sepeda spd1 = new Sepeda();
+        Sepeda spd2 = new Sepeda();
+
+        spd1.setMerek("Polygon");
+        spd1.tambahKecepatan(10);
+        spd1.gantiGear(2);
+        spd1.cetakStatus();
+
+        spd2.setMerek("Whim Cycle");
+        spd2.tambahKecepatan(10);
+        spd2.gantiGear(2);
+        spd2.tambahKecepatan(10);
+        spd2.gantiGear(3);
+        spd2.cetakStatus();
+    }
+
+}
