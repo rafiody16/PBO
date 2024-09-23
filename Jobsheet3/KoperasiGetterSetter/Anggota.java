f60f5c59c800c1178eb61fb39ccf6dbb851afbde
RAFI ODY PRASETYO <rafiody16@gmail.com>
2024-09-23 11:01:25 +0700

    j3

--- Jobsheet3/KoperasiGetterSetter/Anggota.java
+++ Jobsheet3/KoperasiGetterSetter/Anggota.java
@@ -3,29 +3,42 @@ package KoperasiGetterSetter;
 public class Anggota {
 
     private String nama;
-    private String alamat;
+    // private String nik;
+    private String nik;
+    private int limit;
+    private float pinjaman;
     private float simpanan;
 
-    Anggota(String nama, String alamat) {
+    public Anggota(){}
+
+    Anggota(String nik, String nama, int limit) {
         this.nama = nama;
-        this.alamat = alamat;
-        simpanan = 0;
+        this.nik = nik;
+        this.limit = limit;
     }
 
     public void setNama(String nama) {
         this.nama = nama;
     }
 
-    public void setAlamat(String alamat) {
-        this.alamat = alamat;
+    public void setnik(String nik) {
+        this.nik = nik;
+    }
+
+    public void setLimit(int limit) {
+        this.limit = limit;
     }
 
     public String getNama() {
         return nama;
     }
 
-    public String getAlamat() {
-        return alamat;
+    public String getNik() {
+        return nik;
+    }
+
+    public int getLimit() {
+        return limit;
     }
 
     public float getSimpanan() {
@@ -37,7 +50,24 @@ public class Anggota {
     }
 
     public void pinjam(float uang) {
-        simpanan -= uang;
+        if (uang > limit) {
+            System.out.println("Maaf, jumlah pinjaman melebihi limit.");
+        } else {
+            pinjaman += uang;
+        }
+    }
+
+    public void angsur(float uang) {
+        float batasAngsur = 0.1f * pinjaman;
+        if (uang < batasAngsur) {
+            System.out.println("Maaf, angsuran harus 10% dari jumlah pinjaman.");
+        } else {
+            pinjaman -= uang;
+        }
+    }
+
+    public float getPinjaman() {
+        return pinjaman;
     }
     
 }
--- Jobsheet3/KoperasiGetterSetter/KoperasiDemo.java
+++ Jobsheet3/KoperasiGetterSetter/KoperasiDemo.java
@@ -1,17 +1,81 @@
 package KoperasiGetterSetter;
+import java.util.Scanner;
 
 public class KoperasiDemo {
 
     public static void main(String[] args) {
-        Anggota anggota1 = new Anggota("Iwan", "Jalan Rawar");
-        System.out.println("Simpanan "+ anggota1.getNama() +" : Rp. "+ anggota1.getSimpanan());
-        anggota1.setNama("Iwan Setiawan");
-        anggota1.setAlamat("Jalan Sukarno Hatta no 10");
-        anggota1.setor(100000);
-        System.out.println("Simpanan "+ anggota1.getNama() +" : Rp. "+ anggota1.getSimpanan());
+        // Anggota anggota1 = new Anggota("Iwan", "Jalan Rawar");
+        // System.out.println("Simpanan "+ anggota1.getNama() +" : Rp. "+ anggota1.getSimpanan());
+        // anggota1.setNama("Iwan Setiawan");
+        // anggota1.setAlamat("Jalan Sukarno Hatta no 10");
+        // anggota1.setor(100000);
+        // System.out.println("Simpanan "+ anggota1.getNama() +" : Rp. "+ anggota1.getSimpanan());
 
-        anggota1.pinjam(5000);
-        System.out.println("Simpanan "+ anggota1.getNama() +" : Rp. "+ anggota1.getSimpanan());
+        // anggota1.pinjam(5000);
+        // System.out.println("Simpanan "+ anggota1.getNama() +" : Rp. "+ anggota1.getSimpanan());
+
+        // Anggota donny = new Anggota("111333444", "Donny", 5000000);
+
+        // System.out.println("Nama Anggota: "+ donny.getNama());
+        // System.out.println("Limit Pinjaman: "+ donny.getLimit());
+
+        // System.out.println("\nMeminjam uang Rp. 10.000.000... ");
+        // donny.pinjam(10000000);
+        // System.out.println("Jumlah pinjaman saat ini: "+ donny.getPinjaman());
+
+        // System.out.println("\nMeminjam uang Rp. 4.000.000... ");
+        // donny.pinjam(4000000);
+        // System.out.println("Jumlah pinjaman saat ini: "+ donny.getPinjaman());
+
+        // System.out.println("\nMembayar angsuran Rp. 1.000.000... ");
+        // donny.angsur(200000);
+        // System.out.println("Jumlah pinjaman saat ini: "+ donny.getPinjaman());
+
+        // System.out.println("\nMembayar angsuran Rp. 3.000.000... ");
+        // donny.angsur(3000000);
+        // System.out.println("Jumlah pinjaman saat ini: "+ donny.getPinjaman());
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("NIK: ");
+        String nik = sc.next();
+        System.out.print("Nama: ");
+        String nama = sc.next();
+        System.out.print("Limit Pinjaman: ");
+        int limit = sc.nextInt();
+        System.out.println();
+        Anggota agt = new Anggota(nik, nama, limit);
+
+        int pilih;
+
+        do {
+            System.out.println("1. Pinjam\n2. Angsur\n3. Exit");
+            System.out.print("Pilih: ");
+            pilih = sc.nextInt();
+            System.out.println();
+            switch (pilih) {
+                case 1:
+                    System.out.print("Nominal Pinjam: ");
+                    int pjm = sc.nextInt();
+                    agt.pinjam(pjm);
+                    System.out.println("Jumlah pinjaman saat ini: "+ agt.getPinjaman());
+                    System.out.println();
+                    break;
+                case 2:
+                    System.out.print("Nominal Angsur: ");
+                    int angs = sc.nextInt();
+                    agt.angsur(angs);
+                    System.out.println("Jumlah pinjaman saat ini: "+ agt.getPinjaman());
+                    System.out.println();
+                    break;
+                case 3:
+                    System.exit(0);
+                    break;
+                default:
+                    break;
+            }    
+        } while (pilih != 3);
+
+        sc.close();
     }
     
 }
