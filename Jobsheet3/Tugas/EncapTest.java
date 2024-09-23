48ff6507aeab72ea3715a461df404885edb5b35f
RAFI ODY PRASETYO <rafiody16@gmail.com>
2024-09-23 09:52:42 +0700

    j3

---
+++ Jobsheet3/Tugas/EncapTest.java
@@ -0,0 +1,14 @@
+package Tugas;
+
+public class EncapTest {
+
+    public static void main(String[] args) {
+        EncapDemo encap = new EncapDemo();
+        encap.setNama("James");
+        encap.setAge(7);
+
+        System.out.println("Name: "+ encap.getNama());
+        System.out.println("Age: "+ encap.getAge());
+    }
+    
+}
