2e42ef33b99f9d2d0a3118a694b2b7eb6a9aa2ad
RAFI ODY PRASETYO <rafiody16@gmail.com>
2024-10-09 19:37:19 +0700

    j6

--- Jobsheet6/Percobaan1/ClassA.java
+++ Jobsheet6/Percobaan1/ClassA.java
@@ -1,3 +1,4 @@
+package Percobaan1;
 /**
  * ClassA
  */
---
+++ Jobsheet6/Percobaan1/ClassB.java
@@ -0,0 +1,13 @@
+package Percobaan1;
+public class ClassB extends ClassA {
+
+    public int z;
+
+    public void getNilaiZ() {
+        System.out.println("nilai z:"+ z);
+    }
+    public void getJumlah() {
+        System.out.println("Jumlah:"+ (x+y+z));
+    }
+    
+}
