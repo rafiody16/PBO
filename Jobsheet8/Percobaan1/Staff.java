4083b935d8d9405c6a4580acdaefc42f7c5f3268
RAFI ODY PRASETYO <rafiody16@gmail.com>
2024-11-03 19:24:10 +0700

    j9

--- Jobsheet8/Percobaan1/Staff.java
+++ Jobsheet8/Percobaan1/Staff.java
@@ -1,3 +1,38 @@
-public class Staff {
+public class Staff extends Karyawan {
+
+    private int lembur;
+    private double gajiLembur;
+
+    public void setLembur(int lembur) {
+        this.lembur = lembur;
+    }
+
+    public int getLembur() {
+        return lembur;
+    }
+
+    public void setGajiLembur(double gajiLembur) {
+        this.gajiLembur = gajiLembur;
+    }
+
+    public double getGajiLembur() {
+        return gajiLembur;
+    }
+
+    public double getGaji(int lembur, double gajiLembur) {
+        return super.getGaji() + lembur * gajiLembur;
+    }
+
+    public double getGaji() {
+        return super.getGaji() + lembur * gajiLembur;
+    }
+
+    public void lihatInfo() {
+        System.out.println("NIP :" + this.getNip());
+        System.out.println("Nama :" + this.getNama());
+        System.out.println("Golongan :" + this.getGolongan());
+        System.out.println("Jml Lembur :%.0f\n" + this.getLembur());
+        System.out.println("Gaji :%.0f\n" + this.getGaji());
+    }
     
 }
